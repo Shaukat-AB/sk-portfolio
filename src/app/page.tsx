d0e180b90342fbd2861b04ced98940ba8dd5614f
Shaukat Ali <shaukatalibans@gmail.com>
2025-12-20 16:43:26 +0500

    feat: create hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,40 @@
-import { Navbar, FooterFragment } from '@/components';
+import { Navbar, FooterFragment, Button } from '@/components';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="min-h-screen">
-      <header>
+    <div className="min-h-screen py-4 relative">
+      <div className="w-full h-24" />
+
+      <header className="absolute top-4 w-full h-24 max-w-3xl mx-auto">
         <Navbar />
       </header>
 
-      <main>Home</main>
+      <main className="px-4 max-w-3xl mx-auto flex flex-col gap-10">
+        <Hero />
+      </main>
 
-      <footer>
+      <footer className="max-w-3xl mx-auto">
         <FooterFragment />
       </footer>
     </div>
   );
 }
+
+const Hero = () => {
+  return (
+    <section className="max-w-md md:max-w-3xl mx-auto px-4 pt-4 flex flex-col items-center gap-4 text-center">
+      <h1 className="hero-title">Where visions are tailored into reality</h1>
+
+      <div className="px-2 space-y-8">
+        <p className="hero-text">
+          I am Shaukat, a full stack engineer specializing in creating full
+          stack solutions that not only function but also inspire
+        </p>
+        <Button asChild className="mb-8">
+          <Link href="/projects">Shaukat | Featured Projects</Link>
+        </Button>
+      </div>
+    </section>
+  );
+};
