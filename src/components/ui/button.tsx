b895d7d8f7509c81337a5bed4f187dedcb38090b
Shaukat Ali <Shaukatalibans@gmail.com>
2025-12-22 15:57:19 +0500

    update: add new equal size and change colors outline and secondary variant buttons

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -14,14 +14,14 @@ const buttonVariants = cva(
         destructive:
           'bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
         outline:
-          'border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
-        secondary:
-          'bg-secondary text-secondary-foreground hover:bg-secondary/80',
+          'border border-bg-active bg-focus shadow-xs hover:bg-bg-active text-bg-content',
+        secondary: 'bg-bg-content text-bg rounded-full hover:bg-bg-content/80',
         ghost:
           'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
-        link: 'text-primary underline-offset-4 hover:underline',
+        link: 'text-primary-content underline-offset-4 hover:underline',
       },
       size: {
+        equal: 'p-4',
         default: 'h-9 px-4 py-2 has-[>svg]:px-3',
         sm: 'h-8 rounded-full gap-1.5 px-3 has-[>svg]:px-2.5',
         lg: 'h-10 rounded-full px-6 has-[>svg]:px-4',
