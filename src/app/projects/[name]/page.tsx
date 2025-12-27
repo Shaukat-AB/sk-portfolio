e25eda188a6b86aa7b273b5e767205b933db3cc6
Shaukat Ali <Shaukatalibans@gmail.com>
2025-12-27 14:33:56 +0500

    feat: create project page and update mdx components

--- src/app/projects/[name]/page.tsx
+++ src/app/projects/[name]/page.tsx
@@ -1,8 +1,92 @@
+import {
+  ImageCarousel,
+  ProjectButtonGroup,
+  ProjectCard,
+  ProjectsWrapper,
+  Separator,
+} from '@/components';
+import {
+  getProjectByName,
+  getProjectNames,
+  getProjectsMetadata,
+} from '@/lib/projects';
+import { Metadata } from 'next';
+
+export async function generateStaticParams(): Promise<{ name: string }[]> {
+  return getProjectNames().map((pname) => ({
+    name: pname,
+  }));
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ name: string }>;
+}): Promise<Metadata> {
+  const { name } = await params;
+  const { frontmatter: metadata } = await getProjectByName(name);
+
+  if (!metadata?.title) throw new Error(`Metadata undefined! (${name})`);
+
+  return {
+    title: metadata.title,
+    description: metadata.description,
+  };
+}
+
 export default async function Page({
   params,
 }: {
   params: Promise<{ name: string }>;
 }) {
   const { name } = await params;
-  return <div>Projects/ {name}</div>;
+  const { default: Project, frontmatter: metadata } = await getProjectByName(
+    name
+  );
+
+  const relatedProjects = (await getProjectsMetadata())
+    .filter((p) => p.orderIndex != metadata.orderIndex)
+    .slice(0, 2);
+
+  return (
+    <>
+      <div className="flex flex-col items-center gap-4 pb-4">
+        <div className="page-title">
+          <h1>{metadata.title}</h1>
+        </div>
+
+        <ProjectButtonGroup
+          githubLink={metadata.githubLink}
+          demoLink={metadata.demoLink}
+        />
+      </div>
+
+      <div className="px-10">
+        <ImageCarousel />
+      </div>
+
+      <div className="px-10 space-y-6 md:grid md:grid-cols-2 md:items-start">
+        <Project />
+      </div>
+
+      <div className="py-10">
+        <Separator orientation="horizontal" className="m-auto max-w-1/5" />
+      </div>
+
+      {/* Related Projects */}
+      <aside>
+        <div className="text-center text-xl md:text-3xl font-semibold">
+          <h3>Related Projects</h3>
+        </div>
+
+        <ProjectsWrapper>
+          {relatedProjects.map((project) => (
+            <ProjectCard key={project.title} {...project} />
+          ))}
+        </ProjectsWrapper>
+      </aside>
+    </>
+  );
 }
+
+export const dynamicParams = false;
--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,7 +1,25 @@
 import type { MDXComponents } from 'mdx/types';
 import Image, { ImageProps } from 'next/image';
+import { CardContent, CardDescription, CardTitle } from './components';
 
 const components: MDXComponents = {
+  h2: (props) => (
+    <CardTitle className="not-nth-[1]:mt-10 md:nth-[1]:col-span-2">
+      <h2 {...props} />
+    </CardTitle>
+  ),
+  p: (props) => (
+    <CardDescription className="col-span-2">
+      <p {...props}></p>
+    </CardDescription>
+  ),
+  ul: (props) => (
+    <CardContent className="px-10 mt-10">
+      <CardDescription>
+        <ul {...props} className="list-disc space-y-1"></ul>
+      </CardDescription>
+    </CardContent>
+  ),
   img: (props) => (
     <Image sizes="100vw" className="w-full h-auto" {...(props as ImageProps)} />
   ),
