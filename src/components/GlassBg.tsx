bfeccc9c2a725c880b0106794d210b52e02e55db
Shaukat Ali <Shaukatalibans@gmail.com>
2025-12-30 15:32:08 +0500

    fix: GlassBg display that causes two of these appear in large screen.

--- src/components/GlassBg.tsx
+++ src/components/GlassBg.tsx
@@ -8,7 +8,9 @@ export const GlassBg = ({
   const newStyle =
     (variant !== 'sm'
       ? 'md:inline-block md:top-0 md:h-20 md:to-bg-active/95 md:from-bg-active/0 hidden'
-      : 'md:hidden') + className;
+      : 'md:hidden') +
+    ' ' +
+    className;
   return (
     <div
       className={`transition-all backdrop-blur-sm bg-linear-0 from-0% to-75% from-bg-active/95 to-bg-active/0 w-full h-100 fixed z-20 bottom-0 ${newStyle}`}
